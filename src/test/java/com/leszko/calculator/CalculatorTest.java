3fc5f45b60ae83302d2e5acc526c8646e8a1c61e
MR V KAIKATTI SUGUMARAN <vijayfromnamakkal@gmail.com>
2022-01-29 07:20:17 +0530

    Update CalculatorTest.java

--- src/test/java/com/leszko/calculator/CalculatorTest.java
+++ src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 public class CalculatorTest {
      private Calculator calculator = new Calculator();
 
-     @Test
+    // @Test
      public void testSum() {
           assertEquals(5, calculator.sum(2, 3));
      }
