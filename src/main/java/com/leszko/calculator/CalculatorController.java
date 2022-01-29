5e894e1197241722c6d6d0dbb93da389649182a9
MR V KAIKATTI SUGUMARAN <vijayfromnamakkal@gmail.com>
2022-01-29 07:35:17 +0530

    Update CalculatorController.java

--- src/main/java/com/leszko/calculator/CalculatorController.java
+++ src/main/java/com/leszko/calculator/CalculatorController.java
@@ -6,12 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class CalculatorController {
-     @Autowired
-     private Calculator calculator;
+    // @Autowired
+     //private Calculator calculator;
 
      @RequestMapping("/sum")
      String sum(@RequestParam("a") Integer a, 
                 @RequestParam("b") Integer b) {
-          return String.valueOf(calculator.sum(a, b));
+          return "100";
+          //return String.valueOf(calculator.sum(a, b));
      }
 }
