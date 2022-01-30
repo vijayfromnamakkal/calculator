64b23a6701c5e84ac64e2c87897dacf253b02b40
MR V KAIKATTI SUGUMARAN <vijayfromnamakkal@gmail.com>
2022-01-30 21:03:11 +0530

    Update acceptance-test.sh

--- acceptance-test.sh
+++ acceptance-test.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+echo "Unit test start"
 test $(curl localhost:8765/sum?a=1\&b=2) -eq 3
+echo "unit test end"
+echo "UAT starts"
 ./gradlew acceptanceTest -Dcalculator.url=http://localhost:8765
+echo "UAT ends"
